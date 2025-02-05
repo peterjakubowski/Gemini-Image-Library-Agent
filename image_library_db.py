63c0aaf6f078e72abd01052f1f449042ce8f4736
peterjakubowski <97050386+peterjakubowski@users.noreply.github.com>
2025-02-04 17:05:30 -0800

    Create image_library_db.py

---
+++ image_library_db.py
@@ -0,0 +1,35 @@
+# Google Gemini Image Library Agent
+#
+# Author: Peter Jakubowski
+# Date: 2/1/2025
+# Description:
+#
+
+import fastlite
+from schemas import Assets, AssetMetadata, GenerativeMetadata, Embeddings
+
+# =========================
+# ======= Database ========
+# =========================
+
+db = fastlite.database('image_library.db')
+
+# Create core table
+assets = db.t.assets
+if not assets.exists():
+    assets = db.create(Assets, pk="id")
+
+# Create metadata table
+asset_metadata = db.t.asset_metadata
+if not asset_metadata.exists():
+    asset_metadata = db.create(AssetMetadata, pk="id")
+
+# Create generative table
+generative_metadata = db.t.generative_metadata
+if not generative_metadata.exists():
+    generative_metadata = db.create(GenerativeMetadata, pk="id")
+
+# Create embedding table
+embeddings = db.t.embeddings
+if not embeddings.exists():
+    embeddings = db.create(Embeddings)
