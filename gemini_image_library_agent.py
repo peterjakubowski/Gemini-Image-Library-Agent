3ab064c785e6e122fde843a88f6b9904241f0e3c
peterjakubowski <97050386+peterjakubowski@users.noreply.github.com>
2025-02-08 13:12:00 -0800

    Update gemini_image_library_agent.py

--- gemini_image_library_agent.py
+++ gemini_image_library_agent.py
@@ -14,37 +14,37 @@ from agent_tools import GeminiChat, process_response
 # ===== Streamlit Begin ====
 # ==========================
 
-st.header('Gemini Image Library Agent')
+st.header('🖼 Gemini Image Library Agent 🤖')
 
 # ==================
 # === BEGIN CHAT ===
 # ==================
 
-avatars = {"assistant": "👾", "user": "😺"}
+st.session_state.avatars = {"assistant": "🤖", "user": "😺"}
 
 if "chat" not in st.session_state:
     # # Start a new chat
     st.session_state.chat = GeminiChat().chat
 
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [
-        {"role": "assistant", "content": "Hello"}]
+    st.session_state["messages"] = []
 
 for msg in st.session_state.messages:
-    st.chat_message(msg["role"], avatar=avatars[msg["role"]]).write(msg["content"])
+    st.chat_message(msg["role"], avatar=st.session_state.avatars[msg["role"]]).write(msg["content"])
 
 if prompt := st.chat_input():
     st.session_state.messages.append({"role": "user", "content": prompt})
-    st.chat_message("user", avatar=avatars['user']).write(prompt)
-    response = st.session_state.chat.send_message(prompt)
+    st.chat_message("user", avatar=st.session_state.avatars['user']).write(prompt)
 
-    try:
-        message = process_response(response, st.session_state.chat)
-    except ClientError as ce:
-        st.error(ce.__str__())
-    except ServerError as se:
-        st.error(se.__str__())
-    else:
-        for msg in message:
-            st.session_state.messages.append({"role": "assistant", "content": msg})
-            st.chat_message("assistant", avatar=avatars['assistant']).write(msg)
+    with st.spinner("Processing request...", show_time=True):
+        response = st.session_state.chat.send_message(prompt)
+        try:
+            message = process_response(response)
+        except ClientError as ce:
+            st.error(ce.__str__())
+        except ServerError as se:
+            st.error(se.__str__())
+        else:
+            for msg in message:
+                st.session_state.messages.append({"role": "assistant", "content": msg})
+                st.chat_message("assistant", avatar=st.session_state.avatars['assistant']).write(msg)
